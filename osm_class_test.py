db13167d4c06b890c3b9164174a94d4886a1ed25
stwuiwmoc <stwuiwmoc5929@gmail.com>
2022-06-03 19:54:22 +0900

    Q(1, 0)とR(3, 0)輝線から計算した温度決定誤差がexcelでの計算と一致することを確認

--- osm_class_test.py
+++ osm_class_test.py
@@ -4,6 +4,10 @@ import importlib
 
 if __name__ == "__main__":
     importlib.reload(osm)
+
+    N_H3p_ion = 5.0e+15
+    T_hypothesis = 600
+
     instrument_params = osm.InstrumentParameters(
         N_read=100,
         I_dark=20,
@@ -16,14 +20,32 @@ if __name__ == "__main__":
 
     Q_1_0_params = osm.EmissionLineParameters(
         rambda=3.9530e-6,
-        N_H3p=5.0e+15,
+        N_H3p=N_H3p_ion,
         g_ns=4,
         J_prime=1,
         A_if=128.7,
         E_prime=2552.5691,
-        T_hypo=600)
+        T_hypo=T_hypothesis)
+
+    R_3_0_params = osm.EmissionLineParameters(
+        rambda=3.4128e-6,
+        N_H3p=N_H3p_ion,
+        g_ns=4,
+        J_prime=4,
+        A_if=177.6,
+        E_prime=3382.9299,
+        T_hypo=T_hypothesis)
 
     Q_1_0_obs = osm.EmissionLineDisperse(
         emission_line_params=Q_1_0_params,
         instrument_params=instrument_params,
         observation_params=observation_params)
+
+    R_3_0_obs = osm.EmissionLineDisperse(
+        emission_line_params=R_3_0_params,
+        instrument_params=instrument_params,
+        observation_params=observation_params)
+
+    temp_decision = osm.TemperatureFromSpectroscopy(
+        emission_disperse_FD=Q_1_0_obs,
+        emission_disperse_HB=R_3_0_obs)
