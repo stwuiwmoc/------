ebbe8fea90eb03be6fc1ea9ad61441ed4d4e3a3b
stwuiwmoc <stwuiwmoc5929@gmail.com>
2022-06-15 23:40:42 +0900

    TOPICSとESPRITの動作確認を分けて実装

--- osm_class_test.py
+++ osm_class_test.py
@@ -5,10 +5,10 @@ import importlib
 if __name__ == "__main__":
     importlib.reload(osm)
 
-    N_H3p_ion = 5.0e+15
-    T_hypothesis = 600
+    N_H3p_ion = 1.0e+16
+    T_hypothesis = 1000
 
-    instrument_params_for_Q_1_0 = osm.InstrumentParameters(
+    ESPRIT_params_for_Q_1_0 = osm.InstrumentParameters(
         is_ESPRIT=True,
         N_read=100,
         I_dark=20,
@@ -19,13 +19,24 @@ if __name__ == "__main__":
         tau_fi_center=0.9,
         FWHM_fi=266e-9)
 
-    instrument_params_for_R_3_0 = osm.InstrumentParameters(
+    ESPRIT_params_for_R_3_0 = osm.InstrumentParameters(
         is_ESPRIT=True,
         N_read=100,
         I_dark=20,
         G_Amp=9,
         has_fiber=True,
         l_fb=15,
+        rambda_fi_center=3.414e-6,
+        tau_fi_center=0.9,
+        FWHM_fi=17e-9)
+
+    TOPICS_params_for_R_3_0 = osm.InstrumentParameters(
+        is_ESPRIT=False,
+        N_read=100,
+        I_dark=20,
+        G_Amp=9,
+        has_fiber=False,
+        l_fb=None,
         rambda_fi_center=3.414e-6,
         tau_fi_center=0.9,
         FWHM_fi=17e-9)
@@ -57,18 +68,24 @@ if __name__ == "__main__":
         E_prime=3382.9299,
         T_hypo=T_hypothesis)
 
-    Q_1_0_obs = osm.EmissionLineDisperse(
+    Q_1_0_obs_ESPRIT = osm.EmissionLineDisperse(
         emission_line_params=Q_1_0_params,
-        instrument_params=instrument_params_for_Q_1_0,
+        instrument_params=ESPRIT_params_for_Q_1_0,
         telescope_params=telescope_params,
         observation_params=observation_params)
 
-    R_3_0_obs = osm.EmissionLineDisperse(
+    R_3_0_obs_ESPRIT = osm.EmissionLineDisperse(
         emission_line_params=R_3_0_params,
-        instrument_params=instrument_params_for_R_3_0,
+        instrument_params=ESPRIT_params_for_R_3_0,
+        telescope_params=telescope_params,
+        observation_params=observation_params)
+
+    R_3_0_obs_TOPICS = osm.EmissionLineDisperse(
+        emission_line_params=R_3_0_params,
+        instrument_params=TOPICS_params_for_R_3_0,
         telescope_params=telescope_params,
         observation_params=observation_params)
 
     temp_decision = osm.TemperatureFromSpectroscopy(
-        emission_disperse_FD=Q_1_0_obs,
-        emission_disperse_HB=R_3_0_obs)
+        emission_disperse_FD=Q_1_0_obs_ESPRIT,
+        emission_disperse_HB=R_3_0_obs_ESPRIT)
