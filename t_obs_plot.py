33dba9040499d6bd7d7ddf75dc5572ae4df7a6e4
stwuiwmoc <stwuiwmoc5929@gmail.com>
2022-06-20 23:25:21 +0900

    名寄の計算とプロットも追加

--- t_obs_plot.py
+++ t_obs_plot.py
@@ -209,6 +209,20 @@ if __name__ == "__main__":
         T_sky=273,
         tau_sky=0.564)
 
+    Pirika_Oct = osm.TelescopeParameters(
+        T_GBT=273,
+        telescope_diameter=0.6,  # T60とF=12が同じなので、イメージスケールの詳細を実装し終わるまではこの値で代入
+        tau_GBT=0.66,
+        T_sky=273,
+        tau_sky=0.131)
+
+    Pirika_Nov = osm.TelescopeParameters(
+        T_GBT=273,
+        telescope_diameter=0.6,  # T60とF=12が同じなので、イメージスケールの詳細を実装し終わるまではこの値で代入
+        tau_GBT=0.66,
+        T_sky=273,
+        tau_sky=0.286)
+
     obs_1bin = osm.ObservationParameters(
         n_bin_spatial=1,
         t_obs=t_obs_array)
@@ -245,6 +259,31 @@ if __name__ == "__main__":
         telescope_params=T60_PWV5000,
         observation_params=obs_4bin)
 
+    result_1bin_Pirika_Oct = osm.EmissionLineDisperse(
+        emission_line_params=R_3_0,
+        instrument_params=TOPICS,
+        telescope_params=Pirika_Oct,
+        observation_params=obs_1bin)
+
+    result_1bin_Pirika_Nov = osm.EmissionLineDisperse(
+        emission_line_params=R_3_0,
+        instrument_params=TOPICS,
+        telescope_params=Pirika_Nov,
+        observation_params=obs_1bin)
+
+    result_16bin_Pirika_Oct = osm.EmissionLineDisperse(
+        emission_line_params=R_3_0,
+        instrument_params=TOPICS,
+        telescope_params=Pirika_Oct,
+        observation_params=obs_16bin)
+
+    result_16bin_Pirika_Nov = osm.EmissionLineDisperse(
+        emission_line_params=R_3_0,
+        instrument_params=TOPICS,
+        telescope_params=Pirika_Nov,
+        observation_params=obs_16bin)
+
+    # plot start
     # plot T60 PWV=2000
     fig1 = plt.figure(figsize=(12, 10))
     gs1 = fig1.add_gridspec(2, 2)
@@ -306,3 +345,65 @@ if __name__ == "__main__":
     fig2.tight_layout()
 
     fig2.savefig(mkfolder() + "fig2.png")
+
+    # plot Pirika October
+    fig3 = plt.figure(figsize=(12, 10))
+    gs3 = fig3.add_gridspec(2, 2)
+    fig3.suptitle("Pirika, October")
+
+    ax33 = plot_input_data_table_plot(
+        fig=fig3,
+        position=gs3[0:2, 1],
+        emission_line_params=R_3_0,
+        telescope_params=Pirika_Oct,
+        instrument_params=TOPICS,
+        result_1bin=result_1bin_Pirika_Oct)
+
+    ax31 = plot_t_obs_vs_Signal_and_Noise_per_1_pixel(
+        fig=fig3,
+        position=gs3[0, 0],
+        t_obs_array_=t_obs_array,
+        instrument_params=TOPICS,
+        result_1bin=result_1bin_Pirika_Oct)
+
+    ax32 = plot_t_obs_vs_SNR(
+        fig=fig3,
+        position=gs3[1, 0],
+        t_obs_array_=t_obs_array,
+        result_1bin=result_1bin_Pirika_Oct,
+        result_nbin=result_16bin_Pirika_Oct)
+
+    fig3.tight_layout()
+
+    fig3.savefig(mkfolder() + "fig3.png")
+
+    # plot Pirika November
+    fig4 = plt.figure(figsize=(12, 10))
+    gs4 = fig4.add_gridspec(2, 2)
+    fig4.suptitle("Pirika, October")
+
+    ax43 = plot_input_data_table_plot(
+        fig=fig4,
+        position=gs4[0:2, 1],
+        emission_line_params=R_3_0,
+        telescope_params=Pirika_Nov,
+        instrument_params=TOPICS,
+        result_1bin=result_1bin_Pirika_Nov)
+
+    ax41 = plot_t_obs_vs_Signal_and_Noise_per_1_pixel(
+        fig=fig4,
+        position=gs4[0, 0],
+        t_obs_array_=t_obs_array,
+        instrument_params=TOPICS,
+        result_1bin=result_1bin_Pirika_Nov)
+
+    ax42 = plot_t_obs_vs_SNR(
+        fig=fig4,
+        position=gs4[1, 0],
+        t_obs_array_=t_obs_array,
+        result_1bin=result_1bin_Pirika_Nov,
+        result_nbin=result_16bin_Pirika_Nov)
+
+    fig4.tight_layout()
+
+    fig4.savefig(mkfolder() + "fig4.png")
