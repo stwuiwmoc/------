1886916163ab180f07b005f1ac215b3a9c50f130
stwuiwmoc <stwuiwmoc5929@gmail.com>
2022-06-20 22:36:12 +0900

    PWV=2000での透過率とPWV=5000での透過率、両方のプロットを作成

--- t_obs_plot.py
+++ t_obs_plot.py
@@ -49,7 +49,14 @@ if __name__ == "__main__":
         tau_fl_center=0.9,
         FWHM_fl=17e-9)
 
-    T60 = osm.TelescopeParameters(
+    T60_PWV2000 = osm.TelescopeParameters(
+        T_GBT=273,
+        telescope_diameter=0.6,
+        tau_GBT=0.66,
+        T_sky=273,
+        tau_sky=0.744)
+
+    T60_PWV5000 = osm.TelescopeParameters(
         T_GBT=273,
         telescope_diameter=0.6,
         tau_GBT=0.66,
@@ -61,46 +68,63 @@ if __name__ == "__main__":
         t_obs=t_obs_array)
 
     obs_4bin = osm.ObservationParameters(
-        n_bin_spatial=4,
+        n_bin_spatial=2 * 2,
         t_obs=t_obs_array)
 
-    R_3_0_obs_1_bin = osm.EmissionLineDisperse(
+    obs_16bin = osm.ObservationParameters(
+        n_bin_spatial=4 * 4,
+        t_obs=t_obs_array)
+
+    result_1bin_T60_PWV2000 = osm.EmissionLineDisperse(
         emission_line_params=R_3_0,
         instrument_params=TOPICS,
-        telescope_params=T60,
+        telescope_params=T60_PWV2000,
         observation_params=obs_1bin)
 
-    R_3_0_obs_4_bin = osm.EmissionLineDisperse(
+    result_1bin_T60_PWV5000 = osm.EmissionLineDisperse(
         emission_line_params=R_3_0,
         instrument_params=TOPICS,
-        telescope_params=T60,
+        telescope_params=T60_PWV5000,
+        observation_params=obs_1bin)
+
+    result_4bin_T60_PWV2000 = osm.EmissionLineDisperse(
+        emission_line_params=R_3_0,
+        instrument_params=TOPICS,
+        telescope_params=T60_PWV2000,
+        observation_params=obs_4bin)
+
+    result_4bin_T60_PWV5000 = osm.EmissionLineDisperse(
+        emission_line_params=R_3_0,
+        instrument_params=TOPICS,
+        telescope_params=T60_PWV5000,
         observation_params=obs_4bin)
 
     # plot start
     fig1 = plt.figure(figsize=(12, 10))
     gs1 = fig1.add_gridspec(2, 2)
+    fig1.suptitle("T60, PWV=2000")
 
     # data table plot
-    table_text_list = [
+    fig1_table_text_list = [
         ["H3+ parameters", "", ""],
         ["rambda", R_3_0.rambda, "m"],
         ["N_H3p", R_3_0.N_H3p, "m^-2"],
         ["T_hypo", R_3_0.T_hypo, "K"],
         ["", "", ""],
         ["Telescope parameters", "", ""],
-        ["telescope_diameter", T60.telescope_diameter, "m"],
-        ["T_GBT", T60.T_GBT, "K"],
-        ["tau_GBT", T60.tau_GBT, ""],
-        ["T_sky", T60.T_sky, "K"],
-        ["tau_sky", T60.tau_sky, ""],
+        ["telescope_diameter", T60_PWV2000.telescope_diameter, "m"],
+        ["T_GBT", T60_PWV2000.T_GBT, "K"],
+        ["tau_GBT", T60_PWV2000.tau_GBT, ""],
+        ["T_sky", T60_PWV2000.T_sky, "K"],
+        ["tau_sky", T60_PWV2000.tau_sky, ""],
         ["", "", ""],
         ["Observation_parameters", "", ""],
         ["t_obs", "", "s"],
         ["", "", ""],
         ["Intensity", "", ""],
-        ["I_obj", R_3_0_obs_1_bin.I_obj, "W / m^2 / str"],
-        ["I_sky", R_3_0_obs_1_bin.I_sky, "W / m^2 / str"],
-        ["I_GBT", R_3_0_obs_1_bin.I_GBT, "W / m^2 / str"],
+        ["I_obj", result_1bin_T60_PWV2000.I_obj, "W / m^2 / str"],
+        ["I_sky", result_1bin_T60_PWV2000.I_sky, "W / m^2 / str"],
+        ["I_GBT", result_1bin_T60_PWV2000.I_GBT, "W / m^2 / str"],
         ["", "", ""],
         ["Instrument parameters", "", ""],
         ["N_read", TOPICS.N_read, "e-rms"],
@@ -112,7 +136,7 @@ if __name__ == "__main__":
     ax13 = osm.plot_parameter_table(
         fig=fig1,
         position=gs1[0:2, 1],
-        parameter_table=table_text_list,
+        parameter_table=fig1_table_text_list,
         fontsize=8)
 
     # t_obs vs e- plot
@@ -124,13 +148,13 @@ if __name__ == "__main__":
 
     ax11.plot(
         t_obs_array,
-        R_3_0_obs_1_bin.S_all * TOPICS.G_sys,
+        result_1bin_T60_PWV2000.S_all * TOPICS.G_sys,
         label="S_all",
         linestyle=linestyle_signal,
         linewidth=linewidth_signal)
     ax11.plot(
         t_obs_array,
-        R_3_0_obs_1_bin.S_obj * TOPICS.G_sys,
+        result_1bin_T60_PWV2000.S_obj * TOPICS.G_sys,
         label="S_obj",
         linestyle=linestyle_signal,
         linewidth=linewidth_signal)
@@ -139,22 +163,22 @@ if __name__ == "__main__":
     linestyle_noise = "-."
     ax11.plot(
         t_obs_array,
-        np.sqrt(R_3_0_obs_1_bin.S_all * TOPICS.G_sys),
+        np.sqrt(result_1bin_T60_PWV2000.S_all * TOPICS.G_sys),
         label="N_all",
         linestyle=linestyle_noise)
     ax11.plot(
         t_obs_array,
-        np.sqrt(R_3_0_obs_1_bin.S_sky * TOPICS.G_sys),
+        np.sqrt(result_1bin_T60_PWV2000.S_sky * TOPICS.G_sys),
         label="N_sky",
         linestyle=linestyle_noise)
     ax11.plot(
         t_obs_array,
-        np.sqrt(R_3_0_obs_1_bin.S_GBT * TOPICS.G_sys),
+        np.sqrt(result_1bin_T60_PWV2000.S_GBT * TOPICS.G_sys),
         label="N_GBT",
         linestyle=linestyle_noise)
     ax11.plot(
         t_obs_array,
-        np.sqrt(R_3_0_obs_1_bin.S_dark * TOPICS.G_sys),
+        np.sqrt(result_1bin_T60_PWV2000.S_dark * TOPICS.G_sys),
         label="N_dark",
         linestyle=linestyle_noise)
     ax11.plot(
@@ -189,12 +213,12 @@ if __name__ == "__main__":
     # SNR plot
     ax12.plot(
         t_obs_array,
-        R_3_0_obs_1_bin.SNR,
-        label=str(R_3_0_obs_1_bin.n_bin) + " pix binning")
+        result_1bin_T60_PWV2000.SNR,
+        label=str(result_1bin_T60_PWV2000.n_bin) + " pix binning")
     ax12.plot(
         t_obs_array,
-        R_3_0_obs_4_bin.SNR,
-        label=str(R_3_0_obs_4_bin.n_bin) + " pix binning")
+        result_4bin_T60_PWV2000.SNR,
+        label=str(result_4bin_T60_PWV2000.n_bin) + " pix binning")
 
     ax12.grid()
 
@@ -208,3 +232,137 @@ if __name__ == "__main__":
     fig1.tight_layout()
 
     fig1.savefig(mkfolder() + "fig1.png")
+
+    # plot start
+    fig2 = plt.figure(figsize=(12, 10))
+    gs1 = fig2.add_gridspec(2, 2)
+    fig2.suptitle("T60, PWV=5000")
+
+    # data table plot
+    fig2_table_text_list = [
+        ["H3+ parameters", "", ""],
+        ["rambda", R_3_0.rambda, "m"],
+        ["N_H3p", R_3_0.N_H3p, "m^-2"],
+        ["T_hypo", R_3_0.T_hypo, "K"],
+        ["", "", ""],
+        ["Telescope parameters", "", ""],
+        ["telescope_diameter", T60_PWV5000.telescope_diameter, "m"],
+        ["T_GBT", T60_PWV5000.T_GBT, "K"],
+        ["tau_GBT", T60_PWV5000.tau_GBT, ""],
+        ["T_sky", T60_PWV5000.T_sky, "K"],
+        ["tau_sky", T60_PWV5000.tau_sky, ""],
+        ["", "", ""],
+        ["Observation_parameters", "", ""],
+        ["t_obs", "", "s"],
+        ["", "", ""],
+        ["Intensity", "", ""],
+        ["I_obj", result_1bin_T60_PWV5000.I_obj, "W / m^2 / str"],
+        ["I_sky", result_1bin_T60_PWV5000.I_sky, "W / m^2 / str"],
+        ["I_GBT", result_1bin_T60_PWV5000.I_GBT, "W / m^2 / str"],
+        ["", "", ""],
+        ["Instrument parameters", "", ""],
+        ["N_read", TOPICS.N_read, "e-rms"],
+        ["I_dark", TOPICS.I_dark, "e-/s"],
+        ["rambda_fl_center", TOPICS.rambda_fl_center, "m"],
+        ["tau_fl_center", TOPICS.tau_fl_center, ""],
+        ["FWHM_fl", TOPICS.FWHM_fl, "m"]]
+
+    ax23 = osm.plot_parameter_table(
+        fig=fig2,
+        position=gs1[0:2, 1],
+        parameter_table=fig2_table_text_list,
+        fontsize=8)
+
+    # t_obs vs e- plot
+    ax21 = fig2.add_subplot(gs1[0, 0])
+
+    # Signal plot
+    linestyle_signal = "-"
+    linewidth_signal = 3
+
+    ax21.plot(
+        t_obs_array,
+        result_1bin_T60_PWV5000.S_all * TOPICS.G_sys,
+        label="S_all",
+        linestyle=linestyle_signal,
+        linewidth=linewidth_signal)
+    ax21.plot(
+        t_obs_array,
+        result_1bin_T60_PWV5000.S_obj * TOPICS.G_sys,
+        label="S_obj",
+        linestyle=linestyle_signal,
+        linewidth=linewidth_signal)
+
+    # Noise plot
+    linestyle_noise = "-."
+    ax21.plot(
+        t_obs_array,
+        np.sqrt(result_1bin_T60_PWV5000.S_all * TOPICS.G_sys),
+        label="N_all",
+        linestyle=linestyle_noise)
+    ax21.plot(
+        t_obs_array,
+        np.sqrt(result_1bin_T60_PWV5000.S_sky * TOPICS.G_sys),
+        label="N_sky",
+        linestyle=linestyle_noise)
+    ax21.plot(
+        t_obs_array,
+        np.sqrt(result_1bin_T60_PWV5000.S_GBT * TOPICS.G_sys),
+        label="N_GBT",
+        linestyle=linestyle_noise)
+    ax21.plot(
+        t_obs_array,
+        np.sqrt(result_1bin_T60_PWV5000.S_dark * TOPICS.G_sys),
+        label="N_dark",
+        linestyle=linestyle_noise)
+    ax21.plot(
+        t_obs_array,
+        TOPICS.N_read * np.ones(len(t_obs_array)),
+        label="N_read",
+        linestyle=linestyle_noise)
+
+    # FullWell plot
+    ax21.plot(
+        t_obs_array,
+        TOPICS.S_FW_pix * TOPICS.G_sys * np.ones(len(t_obs_array)),
+        label="FW limit",
+        linestyle=":",
+        linewidth=3)
+
+    # axes decoration
+    ax21.grid(axis="x", which="major")
+    ax21.grid(axis="y", which="major")
+
+    ax21.set_xlabel("Integration Time t_obs [s]")
+    ax21.set_ylabel("Signal and Noise per 1 pixel [e-]")
+
+    ax21.set_xscale("log")
+    ax21.set_yscale("log")
+
+    ax21.legend()
+
+    # t_obs vs SNR plot
+    ax22 = fig2.add_subplot(gs1[1, 0])
+
+    # SNR plot
+    ax22.plot(
+        t_obs_array,
+        result_1bin_T60_PWV5000.SNR,
+        label=str(result_1bin_T60_PWV5000.n_bin) + " pix binning")
+    ax22.plot(
+        t_obs_array,
+        result_4bin_T60_PWV5000.SNR,
+        label=str(result_4bin_T60_PWV5000.n_bin) + " pix binning")
+
+    ax22.grid()
+
+    ax22.set_xlabel("Integration Time t_obs [s]")
+    ax22.set_ylabel("SNR of each binning number")
+
+    ax22.set_xscale("log")
+
+    ax22.legend()
+
+    fig2.tight_layout()
+
+    fig2.savefig(mkfolder() + "fig2.png")
