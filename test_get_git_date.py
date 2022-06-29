27f4bbe6f58691d329be1d3276e7da7731de9ce7
stwuiwmoc <stwuiwmoc5929@gmail.com>
2022-06-29 21:06:59 +0900

    cp932だと文字化けで読めないのでutf-8に変更

--- test_get_git_date.py
+++ test_get_git_date.py
@@ -4,4 +4,4 @@ from subprocess import PIPE
 
 if __name__ == "__main__":
     proc = subprocess.run(["git", "show", "HEAD"], stdout=PIPE, stderr=PIPE, shell=True)
-    print(proc.stdout.decode("cp932"))
+    print(proc.stdout.decode("utf-8"))
