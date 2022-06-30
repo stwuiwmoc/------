267101ce372f8b8c8bf0c8e64f8f5b4d512552e8
stwuiwmoc <stwuiwmoc5929@gmail.com>
2022-06-30 11:51:54 +0900

    そもそも日時だけを出力できるgitコマンドに変更

--- test_get_git_date.py
+++ test_get_git_date.py
@@ -2,14 +2,22 @@
 import subprocess
 
 if __name__ == "__main__":
+
+    print("--- Get all ---")
     proc1 = subprocess.run(
         ["git", "show", "HEAD"],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         shell=True)
     proc1_text = proc1.stdout.decode("utf-8")
+    print(proc1_text)
 
-    target = "Date:   "
-    idx = proc1_text.find(target)
-    extracted_text = proc1_text[idx + 12:idx + 38]
-    print(extracted_text)
+    print("--- Get only date---")
+
+    proc2 = subprocess.run(
+        ["git", "log", "-1", "--format='%cI'"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True)
+    proc2_text = proc2.stdout.decode("utf-8")
+    print(proc2_text)
