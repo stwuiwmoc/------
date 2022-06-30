31f8123bad9d22dffe7f9777e451bebef8dcf19f
stwuiwmoc <stwuiwmoc5929@gmail.com>
2022-06-30 11:42:02 +0900

    直前のコミット日時の文字列抽出に成功

--- test_get_git_date.py
+++ test_get_git_date.py
@@ -2,18 +2,14 @@
 import subprocess
 
 if __name__ == "__main__":
-    print("--- start ---")
     proc1 = subprocess.run(
         ["git", "show", "HEAD"],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         shell=True)
-    print(proc1.stdout.decode("utf-8"))
+    proc1_text = proc1.stdout.decode("utf-8")
 
-    print("--- start ---")
-    proc2 = subprocess.run(
-        ["git", "log"],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=True)
-    print(proc2.stdout.decode("utf-8"))
+    target = "Date:   "
+    idx = proc1_text.find(target)
+    extracted_text = proc1_text[idx + 12:idx + 38]
+    print(extracted_text)
