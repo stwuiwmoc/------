cc0e72bef32458a97a741a8d9ccd634e4eca6b30
stwuiwmoc <stwuiwmoc5929@gmail.com>
2022-06-30 17:52:51 +0900

    変更箇所とステージング有無の取得を関数化

--- test_get_git_date.py
+++ test_get_git_date.py
@@ -23,6 +23,56 @@ def get_latest_commit_date() -> str:
     return latest_commit_date_text
 
 
+def get_current_git_status() -> str:
+    """ファイルの変更箇所の有無とステージングの有無を文字列として取得する
+
+    Returns
+    -------
+    str
+        変更箇所の有無とステージングの有無についての文字列
+    """
+    git_command = ["git", "status", "--short"]
+
+    proc = subprocess.run(
+        git_command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True)
+
+    proc_text = proc.stdout.decode("utf-8")
+
+    if len(proc_text) == 0:
+        # git status --short で出力される文字列が無い
+        # つまり「ファイルの変更箇所なし」を意味する
+        text_about_change = "Not changed"
+        text_about_staging = ""
+
+    else:
+        text_about_change = "Changed"
+        # git status --short で出力された文字列がある
+        # つまり「ファイルに何かしらの変更箇所あり」を意味する
+
+        if proc_text[0:2] == " M":
+            # 何もステージングされていない
+            text_about_staging = " and Not staged"
+
+        elif proc_text[0:2] == "MM":
+            # 変更箇所の一部分のみステージングされている
+            text_about_staging = " and Partially staged"
+
+        elif proc_text[0:2] == "M ":
+            # 変更箇所は全てステージングされている
+            text_about_staging = " and All staged"
+
+        else:
+            # 上記に該当しないので、git status で想定外の出力が出ている
+            text_about_staging = " and Can't read current status. Check commands for subprocess."
+
+    text_about_current_git_status = text_about_change + text_about_staging
+
+    return text_about_current_git_status
+
+
 if __name__ == "__main__":
 
     print("--- Get all ---")
@@ -38,29 +88,4 @@ if __name__ == "__main__":
     print(get_latest_commit_date())
 
     print("--- Get change status---")
-    proc3 = subprocess.run(
-        ["git", "status", "--short"],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=True)
-
-    proc3_text = proc3.stdout.decode("utf-8")
-    print(proc3_text)
-
-    if len(proc3_text) == 0:
-        text_about_change = "Not changed"
-        text_about_staging = ""
-
-    else:
-        text_about_change = "Changed"
-
-        if proc3_text[0:2] == " M":
-            text_about_staging = "and Not staged"
-        elif proc3_text[0:2] == "MM":
-            text_about_staging = "and Partially staged"
-        elif proc3_text[0:2] == "M ":
-            text_about_staging = "and All staged"
-        else:
-            text_about_staging = "and Can't read current status. Check commands for subprocess."
-
-    print(text_about_change + text_about_staging)
+    print(get_current_git_status())
