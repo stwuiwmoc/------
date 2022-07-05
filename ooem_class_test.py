58b6ff653fe37d4953a5c0194e205aa032073ba2
stwuiwmoc <stwuiwmoc5929@gmail.com>
2022-07-05 22:16:44 +0900

    問題ないことを確認できたのでテスト用部分を削除

--- ooem_class_test.py
+++ ooem_class_test.py
@@ -77,26 +77,6 @@ if __name__ == "__main__":
         T_GBT=280,
         tau_GBT=0.66)
 
-    # GBTのテスト
-    # プランクの法則の確認用
-    light_test1 = ooem.LightGenenrator(
-        rambda_lower_limit=1e-6,
-        rambda_upper_limit=20e-6,
-        rambda_division_width=1e-9)
-
-    T60.pass_through(light_instance=light_test1)
-    light_test1.show_rambda_vs_I_prime_plot()
-
-    # 絶対値の概算用
-    light_test2 = ooem.LightGenenrator(
-        rambda_lower_limit=3.4128e-6,
-        rambda_upper_limit=3.4128e-6 + 0.1e-9,
-        rambda_division_width=0.1e-9)
-
-    T60.pass_through(light_instance=light_test2)
-    print(light_test2.get_I_prime() * 17e-9)
-    print(light_test2.get_I_prime() * 17e-9 * T60.get_tau_GBT())  # calc_S_xxの計算の中で間違ってI_GBTにも望遠鏡透過率を掛けて過小評価してる
-
     # 輝線発光を加える
     light.show_rambda_vs_I_prime_plot()
 
@@ -119,5 +99,6 @@ if __name__ == "__main__":
     R_4_4.add_auroral_emission_to(light_instance=light)
     light.show_rambda_vs_I_prime_plot()
 
+    # 望遠鏡を通る
     T60.pass_through(light_instance=light)
     light.show_rambda_vs_I_prime_plot()
