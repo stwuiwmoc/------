c8521fc6d61267fa30ce82e0ae148b7a627ed1dc
stwuiwmoc <stwuiwmoc5929@gmail.com>
2022-07-11 18:34:07 +0900

    共にF12のT60とPirikaでAΩは有効数字10桁の精度で一致したので、テストを削除

--- ooem_class_test.py
+++ ooem_class_test.py
@@ -112,18 +112,4 @@ if __name__ == "__main__":
     light.show_rambda_vs_I_prime_plot()
 
     # 望遠鏡への撮像装置の設置
-
-    # F値が変わらなければAΩが変わらないことを確認するためのテスト
     TOPICS.set_ImagingInstrument_to(GBT_instance=T60)
-    print(TOPICS.get_theta_pix())
-    print("A * Omega = ", T60.get_A_GBT() * TOPICS.get_Omega_pix())
-
-    Pirika = ooem.GroundBasedTelescope(
-        D_GBT=1.6,
-        FNO_GBT=12,
-        T_GBT=273,
-        tau_GBT=0.66)
-
-    TOPICS.set_ImagingInstrument_to(GBT_instance=Pirika)
-    print(TOPICS.get_theta_pix())
-    print("A * Omega = ", Pirika.get_A_GBT() * TOPICS.get_Omega_pix())
