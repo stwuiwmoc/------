7f070270ecd3bd949ee9cb39f7bf634397b79eec
stwuiwmoc <stwuiwmoc5929@gmail.com>
2022-07-14 13:56:34 +0900

    章の名前を微修正

--- object_oriented_estimation_myclass.py
+++ object_oriented_estimation_myclass.py
@@ -528,7 +528,7 @@ class GroundBasedTelescope:
         oop観測見積もり.md
             └ 望遠鏡の発光 \n
                 ├ 望遠鏡光学系のパラメータ \n
-                └ 望遠鏡の熱輻射による放射輝度 \n
+                └ 望遠鏡の熱輻射による分光放射輝度 \n
 
         Parameters
         ----------
@@ -589,7 +589,7 @@ class GroundBasedTelescope:
 
             oop観測見積もり.md
                 └ 望遠鏡の発光 \n
-                    └ 望遠鏡の熱輻射による放射輝度 \n
+                    └ 望遠鏡の熱輻射による分光放射輝度 \n
 
             Parameters
             ----------
